85671195c1509c6b455c2c0659db1a926ac6ad42
Pallandos <jean.caylus@ensta.fr>
2025-11-02 15:04:39 +0100

    feat: add EDX feature field + clean code

--- src/features/features.c
+++ src/features/features.c
@@ -36,11 +36,53 @@ typedef enum {
     HYPERVISOR  = 31
 } ECX_Feature;
 
-const char* feature_names[] = {
+typedef enum {
+    FPU          = 0,
+    VME          = 1,
+    DE           = 2,
+    PSE          = 3,
+    TSC          = 4,
+    MSR          = 5,
+    PAE          = 6,
+    MCE          = 7,
+    CX8          = 8,
+    APIC         = 9,
+    // 10 is reserved
+    SEP          = 11,
+    MTRR         = 12,
+    PGE          = 13,
+    MCA          = 14,
+    CMOV         = 15,
+    PAT          = 16,
+    PSE36        = 17,
+    PSN          = 18,
+    CLFLUSH      = 19,
+    // 20 is reserved
+    DS           = 21,
+    ACPI         = 22,
+    MMX          = 23,
+    FXSR         = 24,
+    SSE          = 25,
+    SSE2         = 26,
+    SS           = 27,
+    HTT          = 28,
+    TM           = 29,
+    // 30 is reserved
+    PBE          = 31
+} EDX_Feature;
+
+const char* ecx_feature_names[] = {
     "SSE3", "PCLMULQDQ", "DTES64", "MONITOR", "DS_CPL", "VMX", "SMX", "EST",
     "TM2", "SSSE3", "CNXT_ID", "SDBG", "FMA", "CX16", "XTPR", "PDCM",
     "RESERVED", "PCID", "DCA", "SSE4_1", "SSE4_2", "X2APIC", "MOVBE", "POPCNT",
     "TSC_DEADLINE", "AESNI", "XSAVE", "OSXSAVE", "AVX", "F16C", "RDRAND", "HYPERVISOR"
+};
+
+const char* edx_feature_names[] = {
+    "FPU", "VME", "DE", "PSE", "TSC", "MSR", "PAE", "MCE",
+    "CX8", "APIC", "RESERVED", "SEP", "MTRR", "PGE", "MCA", "CMOV",
+    "PAT", "PSE36", "PSN", "CLFLUSH", "RESERVED", "DS", "ACPI", "MMX",
+    "FXSR", "SSE", "SSE2", "SS", "HTT", "TM", "RESERVED", "PBE"
 };
 
 int cpu_features(){
@@ -49,10 +91,19 @@ int cpu_features(){
     __get_cpuid(1, &unused, &unused, &ecx, &edx);
 
     printf("For each feature, a value of 1 means it is available, 0 means it is not\n");
-    for (int i = 0; i < 32; i++) {
+
+    printf("ECX Features:\n");
+    for (ECX_Feature i = SSE3; i <= HYPERVISOR; i++) {
         int bit = (ecx >> i) & 1;
-        printf("Bit %2d (%-12s): %d\n", i, feature_names[i], bit);
+        printf("Bit %2d (%-12s): %d\n", i, ecx_feature_names[i], bit);
     }
+
+    printf("\nEDX Features:\n");
+    for (EDX_Feature i = FPU; i <= PBE; i++) {
+        int bit = (edx >> i) & 1;
+        printf("Bit %2d (%-12s): %d\n", i, edx_feature_names[i], bit);
+    }
+
 
     return 0;
 }
