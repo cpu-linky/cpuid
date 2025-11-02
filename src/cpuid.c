3cd380362d7facea8badbd0ade83025b04b6b1be
Pallandos <jean.caylus@ensta.fr>
2025-11-02 15:20:58 +0100

    feat: now in a CLI

--- src/cpuid.c
+++ src/cpuid.c
@@ -1,19 +1,38 @@
+#include <stdio.h>
+#include <getopt.h>
 #include "vendor/vendor.h"
 #include "features/features.h"
-#include <stdio.h>
 
-int main(){
-    printf("############ cpuid ############\n");
+void print_help() {
+    printf("Usage: cpuid [options]\n");
+    printf("Options:\n");
+    printf("  -h : Show this help message\n");
+    printf("  -v : Display CPU vendor information\n");
+    printf("  -f : Display CPU features\n");
+}
 
-    // vendor infos
-    printf("===== Vendor =====\n");
-    cpu_vendor();
+int main(int argc, char *argv[]) {
+    int opt;
 
-    printf("\n");
+    if (argc < 2){
+        print_help();
+        return 1;
+    }
 
-    // features :
-    printf("===== Features =====\n");
-    cpu_features();
-
-    return 0;
+    while ((opt = getopt(argc, argv, "hvf")) != -1) {
+        switch (opt) {
+            case 'h':
+                print_help();
+                return 0;
+            case 'v':
+                cpu_vendor();
+                break;
+            case 'f':
+                cpu_features();
+                break;
+            default:
+                print_help();
+                return 1;
+        }
+    }
 }
